8c758ec2d13c2a42460c9e93ba71d7e249f02f09
Sam Kvale <skvale@widen.com>
2016-11-23 18:29:54 -0600

    allow empty receiving ports from ElmApp

--- index.js
+++ index.js
@@ -1,43 +1,49 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
-export default elmApp => class ElmWrapper extends Component {
+export default ElmApp =>
+  class ElmWrapper extends Component {
+    exportingPorts = {}
+    receivingPorts = {}
 
-  constructor (props) {
-    super(props)
-    this.callbackList = []
-    this.data = {}
-  }
+    _initialize = (node) => {
+      if (!node) {
+        return
+      }
 
-  initialize (node) {
-    if (node === null) {
-            // node is unmounting
-      return
+      const app = ElmApp.embed(node, this.props)
+
+      Object.keys(this.props).forEach(propName => {
+        if (typeof this.props[propName] === 'function') {
+          app.ports[propName].subscribe(this.props[propName])
+          this.exportingPorts[propName] = true
+        } else {
+          if (app.ports[propName]) {
+            this.receivingPorts[propName] = data => app.ports[propName].send(data)
+          }
+        }
+      })
     }
 
-    const app = elmApp.embed(node, this.props)
+    _isReceivingPort = (propName) => {
+      return !this.exportingPorts[propName]
+    }
 
-    Object.keys(this.props).forEach(propName => {
-      if (typeof this.props[propName] === 'function') {
-        app.ports[propName].subscribe(this.props[propName])
-        this.callbackList.push(propName)
-      } else {
-        this.data[propName] = data => app.ports[propName].send(data)
-      }
-    })
+    shouldComponentUpdate (nextProps) {
+      Object.keys(nextProps).forEach(propName => {
+        if (this._isReceivingPort(propName)) {
+          const value = nextProps[propName]
+          const sendToElm = this.receivingPorts[propName]
+
+          if (sendToElm && typeof sendToElm === 'function') {
+            sendToElm(value)
+          }
+        }
+      })
+      return false
+    }
+
+    render () {
+      return React.createElement('div', {ref: this._initialize})
+    }
   }
 
-  shouldComponentUpdate (nextProps) {
-    Object.keys(nextProps).forEach(propName => {
-      if (!this.callbackList.includes(propName)) {
-        const value = nextProps[propName]
-        const sendToElm = this.data[propName]
-        sendToElm(value)
-      }
-    })
-    return false
-  }
-
-  render () {
-    return React.createElement('div', {ref: this.initialize.bind(this)})
-  }
-}
